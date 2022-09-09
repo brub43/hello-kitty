164ed05e6768675ed535daedb6c0531bb3895dad
Jordan Hubbard <jordanh@nvidia.com>
2022-09-09 00:14:32 -0700

    Hello Kitty extension.

---
+++ exts/omni.hello.kitty/omni/hello/kitty/tests/test_hello_kitty.py
@@ -0,0 +1,19 @@
+# NOTE:
+#   omni.kit.test - std python's unittest module with additional wrapping to add suport for async/await tests
+#   For most things refer to unittest docs: https://docs.python.org/3/library/unittest.html
+import omni.kit.test
+
+
+# Import extension python module we are testing with absolute import path, as if we are external user (other extension)
+import omni.hello.kitty
+
+
+# Having a test class dervived from omni.kit.test.AsyncTestCase declared on the root of module will make it auto-discoverable by omni.kit.test
+class Test(omni.kit.test.AsyncTestCase):
+    # Before running each test
+    async def setUp(self):
+        pass
+
+    # After running each test
+    async def tearDown(self):
+        pass
